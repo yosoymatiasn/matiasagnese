c2d2a3b176a07099cadc7f3d52636190f9b832df
Matias Agnese <yosoymatiasn@hotmail.com>
2022-12-12 13:03:19 -0500

    added public folder which was missing. fleshed out some config issues that I overlooked in the previous commit. it should now be a good project starter.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
 			<nav>
 				<Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
 				<Link href="/users">Users List</Link> |{" "}
-				<a href="/api/users">Users API</a>
+				<Link href="/api/users">Users API</Link>
 			</nav>
 		</header>
 		{children}
